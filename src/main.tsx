6af7939dc7f7be8408212caa430bdabcca293c9d
Walin Paredes <paolitomaldonadofarje@gmail.com>
2025-06-09 14:48:41 -0500

    Update the font family with @fontsource Geologica

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import { createRoot } from 'react-dom/client'
+import '@fontsource-variable/geologica'
 import './index.css'
 import App from './App.tsx'
+
 
 createRoot(document.getElementById('root')!).render(
   <App />
