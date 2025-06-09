74d781801b9450b4aa3e0d4c92ba266494786708
Walin Paredes <paolitomaldonadofarje@gmail.com>
2025-06-09 14:51:37 -0500

    Refactor CSS styles for logo and notes layout

--- src/types.d.ts
+++ src/types.d.ts
@@ -3,7 +3,7 @@ import type { LABELS } from "./consts"
 export type EventFormType = React.FormEvent<HTMLFormElement>
 
 export interface Note {
-  id: string,
+  id: string
   comment: string
   label: typeof LABELS[keyof typeof LABELS]
 }
@@ -11,4 +11,5 @@ export interface Note {
 export interface Notes { notes: Note[] }
 export type NoteComment = Pick<Note, 'comment'>
 export type NoteLabel = Pick<Note, 'label'>
+export type NoteId = Pick<Note, 'id'>
 export type NoteCommentLabel = Pick<Note, 'comment' | 'label'>
